ae3a5e23bf7b5989983e331805d44fb7c58a5b9e
Erik Freundorfer <e.freundorfer@gmail.com>
2023-09-12 11:55:13 -0400

    RetirerBouleBingo fixed (index decreases in array; to test)

--- TP2 Prog II - Simulation Bingo - Bettina Janesch/Source.cpp
+++ TP2 Prog II - Simulation Bingo - Bettina Janesch/Source.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <stdlib.h> // system cls
-#include <conio.h> // getch
-#include <random> // rand fonc
+#include <curses.h> // getch
+#include <random>	// rand fonc
 
 using namespace std;
 
@@ -11,37 +11,33 @@ struct BouleBingo_s
 	int Numero;
 };
 
-
 struct CaseCarte_s
 {
 	bool Etat;
 	BouleBingo_s BouleCarte;
 };
 
-string LettreBingo[5] = { "B", "I", "N", "G", "O" };
+string LettreBingo[5] = {"B", "I", "N", "G", "O"};
 
 const int NumeroBoules = 75;
 int BoulesRestantsBoulier = NumeroBoules; // variable qui change apres chaque tirage (75,74, ... )
-int BoulesDejaTirees = 0; // variable qui augmente chaque tirage - 0,1,2)
+int BoulesDejaTirees = 0;				  // variable qui augmente chaque tirage - 0,1,2)
 
 BouleBingo_s Boulier[NumeroBoules];
 BouleBingo_s BoulesApresTirage[NumeroBoules];
 BouleBingo_s BouleChoisi;
 
-
 CaseCarte_s CarteBingo[5][5];
 
 void AffichageMenu();
-void BoulierInitial(BouleBingo_s tab[], const int taille); // boulier plein
-void AfficherBoulier(BouleBingo_s tab[], const int taille); // affiche le boulier plein qui se vide avec chaquE tirage
-void BoulesTirees(BouleBingo_s tab[], const int taille); // tableau initial vide qui se remplit avec chaque tirage
-void CarteInitiale(CaseCarte_s matrice[5][5]); // carte initiale
-void AfficherCarte(CaseCarte_s matrice[5][5]); // affiche matrice 5x5 nouvelle
-int BoulierHasard(int &NombreMaximum); // pige numéro au hasard (max 75; apres ca descend)
-BouleBingo_s RetirerBouleBingo(BouleBingo_s tab[], const int taille, int IndiceRandomtrouve); //Réarrange Boulier; boule est retirée; renvoyé a la fin du tableau et initialiser comme 0
+void BoulierInitial(BouleBingo_s tab[], const int taille);									  // boulier plein
+void AfficherBoulier(BouleBingo_s tab[], const int taille);									  // affiche le boulier plein qui se vide avec chaquE tirage
+void BoulesTirees(BouleBingo_s tab[], const int taille);									  // tableau initial vide qui se remplit avec chaque tirage
+void CarteInitiale(CaseCarte_s matrice[5][5]);												  // carte initiale
+void AfficherCarte(CaseCarte_s matrice[5][5]);												  // affiche matrice 5x5 nouvelle
+int BoulierHasard(int &NombreMaximum);														  // pige numï¿½ro au hasard (max 75; apres ca descend)
+BouleBingo_s RetirerBouleBingo(BouleBingo_s tab[], const int taille, int IndiceRandomtrouve); // Rï¿½arrange Boulier; boule est retirï¿½e; renvoyï¿½ a la fin du tableau et initialiser comme 0
 void AjouterBouleBingo(BouleBingo_s tab[], int &DejaTire, BouleBingo_s BingoPourAjouter);
-
-
 
 void main(void)
 {
@@ -57,7 +53,7 @@ void main(void)
 		OptionMenu = toupper(OptionMenu);
 		switch (OptionMenu)
 		{
-		case 'T': 
+		case 'T':
 			BouleChoisi = RetirerBouleBingo(Boulier, NumeroBoules, BoulierHasard(BoulesRestantsBoulier));
 			AjouterBouleBingo(BoulesApresTirage, BoulesDejaTirees, BouleChoisi);
 			_getch();
@@ -65,43 +61,41 @@ void main(void)
 		case 'B':
 			system("cls");
 			cout << "\n\n*****************"
-				"\n*** BINGO !!! ***"
-				"\n*****************";
+					"\n*** BINGO !!! ***"
+					"\n*****************";
 			_getch();
 			break;
 		case 'Q':
 			_getch();
 		}
-	}
-	while (OptionMenu != 'Q');
+	} while (OptionMenu != 'Q');
 
-	//BoulierInitial(Boulier, NumeroBoules);
-	//AfficherBoulier(Boulier, NumeroBoules);
-	//BouleBingo_s BouleChoisi = RetirerBouleBingo(Boulier, NumeroBoules, BoulierHasard(BoulesRestantsBoulier));// indice random trouve = fonction Boulier Hasard
-	//AfficherBoulier(Boulier, NumeroBoules);
-	//AjouterBouleBingo(BoulesApresTirage, BoulesDejaTirees, BouleChoisi);
-	//AfficherBoulier(BoulesApresTirage, NumeroBoules);
-
+	// BoulierInitial(Boulier, NumeroBoules);
+	// AfficherBoulier(Boulier, NumeroBoules);
+	// BouleBingo_s BouleChoisi = RetirerBouleBingo(Boulier, NumeroBoules, BoulierHasard(BoulesRestantsBoulier));// indice random trouve = fonction Boulier Hasard
+	// AfficherBoulier(Boulier, NumeroBoules);
+	// AjouterBouleBingo(BoulesApresTirage, BoulesDejaTirees, BouleChoisi);
+	// AfficherBoulier(BoulesApresTirage, NumeroBoules);
 }
-	
+
 void AffichageMenu()
 {
 	system("cls");
 	cout << "*********************************************    MENU    **************************************************"
-		"\n***\t\t\tT - Tirer un numéro\tB - BINGO!\tQ - Quitter le menu\t\t\t***"
-		"\n***********************************************************************************************************\n";
-	
+			"\n***\t\t\tT - Tirer un numï¿½ro\tB - BINGO!\tQ - Quitter le menu\t\t\t***"
+			"\n***********************************************************************************************************\n";
+
 	cout << "BOULIER\n\n";
 	AfficherBoulier(Boulier, BoulesRestantsBoulier);
 	cout << "\n\n"
-		"TIRAGE DE LA BOULE:\n";
+			"TIRAGE DE LA BOULE:\n";
 	cout << BouleChoisi.Lettre << BouleChoisi.Numero;
-	cout << "\n\nBOULES DÈJÀ TIRÉES\n";
-	
+	cout << "\n\nBOULES Dï¿½Jï¿½ TIRï¿½ES\n";
+
 	AfficherBoulier(BoulesApresTirage, NumeroBoules);
 	CarteInitiale(CarteBingo);
 	AfficherCarte(CarteBingo);
-}	
+}
 
 void BoulierInitial(BouleBingo_s tab[], const int taille)
 {
@@ -109,23 +103,23 @@ void BoulierInitial(BouleBingo_s tab[], const int taille)
 	{
 		if (i < 15)
 		{
-			Boulier[i] = { LettreBingo[0], i + 1 };
+			Boulier[i] = {LettreBingo[0], i + 1};
 		}
 		else if (i >= 15 && i < 30)
 		{
-			Boulier[i] = { LettreBingo[1], i + 1 };
+			Boulier[i] = {LettreBingo[1], i + 1};
 		}
 		else if (i >= 30 && i < 46)
 		{
-			Boulier[i] = { LettreBingo[2], i + 1 };
+			Boulier[i] = {LettreBingo[2], i + 1};
 		}
 		else if (i >= 46 && i < 60)
 		{
-			Boulier[i] = { LettreBingo[3], i + 1 };
+			Boulier[i] = {LettreBingo[3], i + 1};
 		}
 		else if (i >= 60 && i <= 75)
 		{
-			Boulier[i] = { LettreBingo[4], i + 1 };
+			Boulier[i] = {LettreBingo[4], i + 1};
 		}
 	}
 }
@@ -158,35 +152,35 @@ void CarteInitiale(CaseCarte_s matrice[5][5])
 	{
 		for (int colonne = 0; colonne < 5; colonne++) // ligne 0,1,2,3,4
 		{
-			if (colonne == 0) //B - 11-15 (tab 10-14)
+			if (colonne == 0) // B - 11-15 (tab 10-14)
 			{
 				matrice[colonne][ligne].BouleCarte.Lettre = LettreBingo[0];
 				matrice[colonne][ligne].BouleCarte.Numero = NumeroB;
 				matrice[colonne][ligne].Etat = false;
 				NumeroB++;
 			}
-			else if (colonne == 1) //I - 16-20 (tab 15-19)
+			else if (colonne == 1) // I - 16-20 (tab 15-19)
 			{
 				matrice[colonne][ligne].BouleCarte.Lettre = LettreBingo[1];
 				matrice[colonne][ligne].BouleCarte.Numero = NumeroI;
 				matrice[colonne][ligne].Etat = false;
 				NumeroI++;
 			}
-			else if (colonne == 2) //N - 31-35 (tab 30-34)
+			else if (colonne == 2) // N - 31-35 (tab 30-34)
 			{
 				matrice[colonne][ligne].BouleCarte.Lettre = LettreBingo[2];
 				matrice[colonne][ligne].BouleCarte.Numero = NumeroN;
 				matrice[colonne][ligne].Etat = false;
 				NumeroN++;
 			}
-			else if (colonne == 3) //G - 46-50 (tab 45-49)
+			else if (colonne == 3) // G - 46-50 (tab 45-49)
 			{
 				matrice[colonne][ligne].BouleCarte.Lettre = LettreBingo[3];
 				matrice[colonne][ligne].BouleCarte.Numero = NumeroG;
 				matrice[colonne][ligne].Etat = false;
 				NumeroG++;
 			}
-			else if (colonne == 4) //O - 61-65 (tab 60-64)
+			else if (colonne == 4) // O - 61-65 (tab 60-64)
 			{
 				matrice[colonne][ligne].BouleCarte.Lettre = LettreBingo[4];
 				matrice[colonne][ligne].BouleCarte.Numero = NumeroO;
@@ -200,7 +194,7 @@ void CarteInitiale(CaseCarte_s matrice[5][5])
 void AfficherCarte(CaseCarte_s matrice[5][5])
 {
 	cout << "\n\n\n=====================================\n"
-		"||  B  ||  I  ||  N  ||  G  ||  O  ";
+			"||  B  ||  I  ||  N  ||  G  ||  O  ";
 	for (int colonne = 0; colonne < 5; colonne++) // col 0: B, col 1: I, col 2: N, col 3: G, col 4: O
 	{
 		for (int ligne = 0; ligne < 5; ligne++) // ligne 0,1,2,3,4
@@ -208,9 +202,9 @@ void AfficherCarte(CaseCarte_s matrice[5][5])
 			if (ligne == 0)
 			{
 				cout << "||";
-				cout<<"\n=====================================";
+				cout << "\n=====================================";
 				cout << "\n";
-				cout << "|| "<<matrice[colonne][ligne].BouleCarte.Numero << "  ";
+				cout << "|| " << matrice[colonne][ligne].BouleCarte.Numero << "  ";
 			}
 			else
 			{
@@ -219,11 +213,10 @@ void AfficherCarte(CaseCarte_s matrice[5][5])
 		}
 	}
 	cout << "||"
-	"\n=====================================";
-
+			"\n=====================================";
 }
 
-int BoulierHasard(int &NombreMaximum) // decrementer NombreMaximum pas ICI car tu veux conserver nbmax et decrementer dans une 
+int BoulierHasard(int &NombreMaximum) // decrementer NombreMaximum pas ICI car tu veux conserver nbmax et decrementer dans une
 {
 	int x = 0;
 	srand(time(NULL));
@@ -234,13 +227,12 @@ int BoulierHasard(int &NombreMaximum) // decrementer NombreMaximum pas ICI car t
 
 BouleBingo_s RetirerBouleBingo(BouleBingo_s tab[], const int taille, int IndiceRandomtrouve)
 {
-	BouleBingo_s Temp = tab[IndiceRandomtrouve];//-1 car tab[75] existe pas
+	BouleBingo_s Temp = tab[IndiceRandomtrouve]; //-1 car tab[75] existe pas
 	tab[IndiceRandomtrouve].Lettre = " ";
 	tab[IndiceRandomtrouve].Numero = 0;
 	for (int i = IndiceRandomtrouve; i < BoulesRestantsBoulier; i++)
 	{
 		tab[i] = tab[i + 1];
-
 	}
 	cout << Temp.Lettre << Temp.Numero;
 	BoulesRestantsBoulier--; // decrementer a 74, 73 ... etc
